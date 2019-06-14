62daaa03f700ba49cdff4c90f079dccdc6131083
Simon <simonxd@gmail.com>
2019-06-14 04:33:09 -0300

    Adds url validators

--- src/validators/url.js
+++ src/validators/url.js
@@ -1,21 +1,21 @@
 import isUrl from 'is-url'
 
 export const url = ({}, value) => {
-  return isUrl(value)
+  return typeof value === 'string' && isUrl(value)
 }
 
 export const urlProtocolAllow = ({ protocols }, value) => {
-
+  return url({}, value) && protocols.indexOf((new URL(value)).protocol) !== -1
 }
 
 export const urlProtocolReject = ({ protocols }, value) => {
-
+  return url({}, value) && protocols.indexOf((new URL(value)).protocol) === -1
 }
 
-export const urlDomainAllow = ({ domains }, value) => {
-  return url({}, value)
+export const urlHostnameAllow = ({ hostnames }, value) => {
+  return url({}, value) && hostnames.indexOf((new URL(value)).hostname) !== -1
 }
 
-export const urlDomainReject = ({ domains }, value) => {
-  return url({}, value)
+export const urlHostnameReject = ({ hostnames }, value) => {
+  return url({}, value) && hostnames.indexOf((new URL(value)).hostname) === -1
 }
