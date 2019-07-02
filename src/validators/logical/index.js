08ca48efc810ba6b36e5e982550ed036705b9e22
Simon <simonxd@gmail.com>
2019-07-02 03:41:35 -0300

    Initiates development for when validators

--- src/validators/logical/index.js
+++ src/validators/logical/index.js
@@ -1,10 +1,13 @@
 import { and } from './and'
 import { or } from './or'
+import { when } from './when'
 
 export * from './and'
 export * from './or'
+export * from './when'
 
 export const LOGICAL_VALIDATORS = {
   and,
-  or
+  or,
+  when
 }
