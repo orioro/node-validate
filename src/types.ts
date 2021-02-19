5c143c9a924b1b40bff7cccadf81b8f8a76dd39b
Simon Fan <simon@orioro.design>
2021-02-19 18:07:39 -0300

    fix: fix types and validateThrow typo

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,23 @@
-import { BooleanExpression } from '@orioro/expression'
+import { Expression } from '@orioro/expression'
 
 /**
  * @typedef {Object} ValidationErrorSpec
  * @property {Object} validationErrorSpec
  * @property {string} validationErrorSpec.code Should be a computer-friendly string
  *                                             which helps identifying the error.
- *                                             Usually not shown to the end-user. 
+ *                                             Usually not shown to the end-user.
  * @property {string | *} [validationErrorSpec.messsage] Should be a human-friendly string
  *                                                       that might be showdn to the end-user
  *                                                       and explain the error's circumstances
  *                                                       and possible solutions.
  */
 export type ValidationErrorSpec = {
-  code: string,
-  message?: string | any, // Allow {any} in order to support custom message generators / formats
+  code: string
+  message?: string | any // Allow {any} in order to support custom message generators / formats
   [key: string]: any
 }
 
 /**
- * @typedef {[BooleanExpression, ValidationErrorSpec]} ValidationCase
+ * @typedef {[Expression, ValidationErrorSpec]} ValidationCase
  */
-export type ValidationCase = [BooleanExpression, ValidationErrorSpec | string]
+export type ValidationCase = [Expression, ValidationErrorSpec | string]
--- src/validate.ts
+++ src/validate.ts
@@ -6,21 +6,18 @@ import {
 } from '@orioro/expression'
 import { ValidationError } from './errors'
 
-import {
-  ValidationErrorSpec,
-  ValidationCase
-} from './types'
+import { ValidationErrorSpec } from './types'
 
 /**
  * Options for calling `validate`
- * 
+ *
  * @typedef {Object} ValidateOptions
  * @property {Object} options
  * @property {Object} options.interpreters Expression interpreters to be passed onto
  *                                         to @orioro/expression
  */
 export type ValidateOptions = {
-  interpreters: { [key: string]: ExpressionInterpreter },
+  interpreters: { [key: string]: ExpressionInterpreter }
 }
 const DEFAULT_VALIDATE_OPTIONS = {
   interpreters: ALL_EXPRESSIONS,
@@ -32,32 +29,23 @@ const DEFAULT_VALIDATE_OPTIONS = {
  * (either `ValidationErrorSpec`, `string`, `null`) and returns
  * either `null` or a non-empty array of objects conforming to
  * `ValidationErrorSpec`.
- * 
+ *
  * @function normalizeValidationResult
  * @param {ValidationErrorSpec | string | null | (ValidationErrorSpec | string)[]} result
  * @returns {ValidationErrorSpec[] | null}
  */
 export const normalizeValidationResult = (
-  result:(
-    ValidationErrorSpec |
-    string |
-    null |
-    (ValidationErrorSpec | string)[]
-  )
-):(ValidationErrorSpec[] | null) => {
+  result: ValidationErrorSpec | string | null | (ValidationErrorSpec | string)[]
+): ValidationErrorSpec[] | null => {
   return typeof result === 'string'
     ? [{ code: result }]
     : Array.isArray(result)
-      ? result.length === 0
-        ? null
-        : result.map(r => (
-            typeof r === 'string'
-              ? { code: r }
-              : r
-          ))
-      : result === null
-        ? null
-        : [result]
+    ? result.length === 0
+      ? null
+      : result.map((r) => (typeof r === 'string' ? { code: r } : r))
+    : result === null
+    ? null
+    : [result]
 }
 
 /**
@@ -67,7 +55,7 @@ export const normalizeValidationResult = (
  * value is valid).
  *
  * The expression may return one of these values:
- * 
+ *
  * - a `string`: is interpreted as an error code and will be
  *   converted to a `ValidationErrorSpec` (e.g. `'SOME_VALIDATION_ERROR'`
  *   becomes `{ code: 'SOME_VALIDATION_ERROR' }`)
@@ -75,7 +63,7 @@ export const normalizeValidationResult = (
  *   have the properties `code` and `message`
  * - an `Array`: is interpreted as an array of `ValidationErrorSpec` objects
  * - `null`: indicates that no errors were found
- * 
+ *
  * @function validate
  * @param {Expression} validationExpression Expression to be evaluated by `@orioro/expression` module.
  * @param {*} value Value against which validation should be run
@@ -84,14 +72,17 @@ export const normalizeValidationResult = (
  * @returns {null | ValidationErrorSpec[]}
  */
 export const validate = (
-  validationExpression:Expression,
-  value:any,
-  { interpreters }:ValidateOptions = DEFAULT_VALIDATE_OPTIONS
-):(ValidationErrorSpec[] | null) => {
-  const result = evaluate({
-    interpreters,
-    scope: { $$VALUE: value }
-  }, validationExpression)
+  validationExpression: Expression,
+  value: any, // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
+  { interpreters }: ValidateOptions = DEFAULT_VALIDATE_OPTIONS
+): ValidationErrorSpec[] | null => {
+  const result = evaluate(
+    {
+      interpreters,
+      scope: { $$VALUE: value },
+    },
+    validationExpression
+  )
 
   return normalizeValidationResult(result)
 }
@@ -99,7 +90,7 @@ export const validate = (
 /**
  * Performs same validation process as `validate` but if an error
  * is encountered throws a `ValidationError`.
- * 
+ *
  * @function validateThrow
  * @param {Expression} validationExpression
  * @param {*} value
@@ -107,13 +98,13 @@ export const validate = (
  * @throws {ValidationError} validationError
  */
 export const validateThrow = (
-  validationExpression:Expression,
-  value:any,
-  options:ValidateOptions = DEFAULT_VALIDATE_OPTIONS
-):void => {
-  const error = validate(validationExpression, value, options)
+  validationExpression: Expression,
+  value: any, // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
+  options: ValidateOptions = DEFAULT_VALIDATE_OPTIONS
+): void => {
+  const errors = validate(validationExpression, value, options)
 
-  if (error !== null) {
-    throw new ValidationError([error], value)
+  if (errors !== null) {
+    throw new ValidationError(errors, value)
   }
 }
